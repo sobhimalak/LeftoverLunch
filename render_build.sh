2c62ffea21a43c7f0993e51e1368e9c0f31f7167
Sobhi Malk <sobhi@cloudulu.com>
2026-03-31 18:25:38 +0300

    debug build by logging to static file

--- render_build.sh
+++ render_build.sh
@@ -8,5 +8,11 @@ pip install django-unfold
 
 cd leftoverlunch
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+# Redirect all output to a log file
+LOG_FILE="static/build_log.txt"
+echo "Build started at $(date)" > $LOG_FILE
+
+python manage.py collectstatic --no-input >> $LOG_FILE 2>&1
+python manage.py migrate >> $LOG_FILE 2>&1
+
+echo "Build finished at $(date)" >> $LOG_FILE
