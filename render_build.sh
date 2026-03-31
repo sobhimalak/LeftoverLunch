1964ce9a8881a7b5b87d34271e002432dfb052b3
Sobhi Malk <sobhi@cloudulu.com>
2026-03-31 17:31:38 +0300

    move deployment files to root, add django-unfold, fix paths

--- render_build.sh
+++ render_build.sh
@@ -2,7 +2,11 @@
 # exit on error
 set -o errexit
 
+# Install dependencies from root
 pip install -r requirements.txt
+pip install django-unfold
+
+cd leftoverlunch
 
 python manage.py collectstatic --no-input
 python manage.py migrate
