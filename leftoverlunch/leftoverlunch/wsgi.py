b3d9feded26009be82f6ff1aa031ae0a49a3dfe9
Sobhi Malk <sobhi@cloudulu.com>
2026-04-01 11:00:49 +0300

    Final production fix: explicit WSGI wrapping and start command adjustment

--- leftoverlunch/leftoverlunch/wsgi.py
+++ leftoverlunch/leftoverlunch/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leftoverlunch.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
