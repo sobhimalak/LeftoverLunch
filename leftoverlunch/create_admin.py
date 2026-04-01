a0c6c1068d272af0016af61b062659ab0c945dee
Sobhi Malk <sobhi@cloudulu.com>
2026-04-01 10:22:30 +0300

    force update admin password in build

--- leftoverlunch/create_admin.py
+++ leftoverlunch/create_admin.py
@@ -15,4 +15,7 @@ if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username, email, password)
     print(f"Superuser created successfully: {username}")
 else:
-    print(f"Superuser {username} already exists")
+    u = User.objects.get(username=username)
+    u.set_password(password)
+    u.save()
+    print(f"Superuser {username} password updated successfully")
